b77ac63edab8a7449568c4ca7cb38d231cc47294
Remi <dufeu.remi@gmail.com>
2023-02-15 17:10:03 +0100

    display list

--- WPF.Reader/ViewModel/ListBook.cs
+++ WPF.Reader/ViewModel/ListBook.cs
@@ -13,7 +13,6 @@ namespace WPF.Reader.ViewModel
 
         public ICommand ItemSelectedCommand { get; set; }
 
-        // n'oublier pas faire de faire le binding dans ListBook.xaml !!!!
         public ObservableCollection<Book> Books => Ioc.Default.GetRequiredService<LibraryService>().Books;
 
         public ListBook()
