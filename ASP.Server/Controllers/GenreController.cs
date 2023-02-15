8aa59a02d6dca92ba54304b8520c926af557901e
Mouncif <mouncif.matallah@etu.univ-cotedazur.fr>
2023-02-15 15:13:25 +0100

    AddGenre

--- ASP.Server/Controllers/GenreController.cs
+++ ASP.Server/Controllers/GenreController.cs
@@ -6,9 +6,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace ASP.Server.Controllers
 {
+    public class CreateGenreModel
+    {
+        [Required]
+        [Display(Name = "Nom")]
+        public string Name { get; set; }
+        public IEnumerable<Book> AllBooks { get; init; }
+
+
+    }
     public class GenreController : Controller
     {
         private readonly LibraryDbContext libraryDbContext;
@@ -18,6 +29,39 @@ namespace ASP.Server.Controllers
             this.libraryDbContext = libraryDbContext;
         }
 
+        public ActionResult<IEnumerable<Genre>> List()
+        {
+            // récupérer les livres dans la base de donées pour qu'elle puisse être affiché
+            List<Genre> ListGenres = null;
+            return View(ListGenres);
+        }
+
         // A vous de faire comme BookController.List mais pour les genres !
+
+        public ActionResult<CreateGenreModel> Create(CreateGenreModel model) 
+
+        {
+         if (ModelState.IsValid)
+            {
+                var genre = new Genre
+                {
+                    Name = model.Name
+                };
+
+                libraryDbContext.Genre.Add(genre);
+                libraryDbContext.SaveChanges();
+
+            }
+
+            var allBooks = libraryDbContext.Books.ToList();
+            var createGenreModel = new CreateGenreModel { AllBooks = allBooks };
+
+            return View(new CreateGenreModel() { AllBooks = libraryDbContext.Books.ToList() });
+
+
+
+        }
     }
+
+   
 }
